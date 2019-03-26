17b0089ddba6c926b30bed5ae2f2a2b1133ba412
Mario S. Alves <malves.dev@gmail.com>
2019-03-26 13:15:01 -0300

    Update setProxy.bat
    Ajuste de url de proxy

--- setProxy.bat
+++ setProxy.bat
@@ -4,5 +4,5 @@ git config --local --unset http.proxy
 git config --local --unset https.proxy
 
 @REM Ajusta os proxys
-git config --local https.proxy https://proxylatam.indra.es:8080
-git config --local http.proxy http://proxylatam.indra.es:8080
+git config --local https.proxy https://dominio.site.info:8080
+git config --local http.proxy http://dominio.site.info:8080
